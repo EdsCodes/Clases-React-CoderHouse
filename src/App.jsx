def59cc3803fb60057b435d9cf887f9dcd27b2b7
Javier Veron <javier.veron@gmail.com>
2024-03-27 22:35:49 -0300

    Clase 2

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Hero from './Hero'
+import Destacados from './Destacados'
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Destacados />
+    </>
+  )
+}
+
+export default App
